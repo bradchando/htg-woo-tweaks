5f8864abd925df555d7de2007da645b738287994
Brad Chandonnet <brad@bradchandonnet.info>
2014-12-04 16:59:37 -0500

    code formatting

--- htg-woo-tweaks.php
+++ htg-woo-tweaks.php
@@ -1,78 +1,78 @@
-	<?php
-	/*
-	Plugin Name: HTG Woo Tweaks
-	Description: Extends the functionality of basic WordPress and WooCommerce to meet the needs of the Hometown Giving business model
-	Version: 1.1
-	Author: Brad Chandonnet
-	Author URI: http://bradchandonnet.com
-	License: GPLv2
-	*/
-	
-	//This function disallows the Processing Fee if a business membership is in the cart
-		
-	add_filter( "woocommerce_pay4pay_apply", 'htg_allow_fee' , 6 , 0 );
-	
-	function htg_allow_fee() {
-		
-		global $woocommerce;
-		
-		$htg_bus_membership_id = 313; //This is the ID of the membership product (I know it's hard coded!)
-		 
-		foreach($woocommerce->cart->get_cart() as $cart_item_key => $values ) {
-	        
-			$_product = $values['data'];
-		
-			if( $htg_bus_membership_id == $_product->id ) {		//A business membership is in the cart
-	            
-	            return false; //Do not charge the fee
-	            
-    		}
-    	
-    	}
-    		     
-	    return true; //Charge the fee
-	}
+<?php
+/*
+Plugin Name: HTG Woo Tweaks
+Description: Extends the functionality of basic WordPress and WooCommerce to meet the needs of the Hometown Giving business model
+Version: 1.1
+Author: Brad Chandonnet
+Author URI: http://bradchandonnet.com
+License: GPLv2
+*/
 
+//This function disallows the Processing Fee if a business membership is in the cart
+	
+add_filter( "woocommerce_pay4pay_apply", 'htg_allow_fee' , 6 , 0 );
 
-
-	//This function changes the coupon message in the checkout screen
-		
-	add_filter( 'woocommerce_checkout_coupon_message', 'htg_rename_coupon_message_on_checkout' );
+function htg_allow_fee() {
 	
-	function htg_rename_coupon_message_on_checkout() {
-		
-		 return '<a href="#" class="showcoupon">Redeeming A Hometown Giving Gift Credit? -- Click here to enter your code.</a>';
-    	
-    }
-
+	global $woocommerce;
 	
-	
-	//This function hides any memberships from the shop page
-	
-	add_action( 'pre_get_posts', 'custom_pre_get_posts_query' );
- 
-	function custom_pre_get_posts_query( $q ) {
+	$htg_bus_membership_id = 313; //This is the ID of the membership product (I know it's hard coded!)
 	 
-		if ( ! $q->is_main_query() ) return;
-		
-		if ( ! $q->is_post_type_archive() ) return;
-		
-		if ( ! is_admin() && is_shop() ) {
-		 
-			$q->set( 'tax_query', array(array(
-				
-				'taxonomy' => 'product_cat',
-				'field' => 'slug',
-				'terms' => array( 'memberships' ),  // Don't display products in the memberships category on the shop page
-				'operator' => 'NOT IN'
-			
-			)));
-			
+	foreach($woocommerce->cart->get_cart() as $cart_item_key => $values ) {
+        
+		$_product = $values['data'];
+	
+		if( $htg_bus_membership_id == $_product->id ) {		//A business membership is in the cart
+            
+            return false; //Do not charge the fee
+            
 		}
-	 
-		remove_action( 'pre_get_posts', 'custom_pre_get_posts_query' );
-	 
+	
 	}
+		     
+    return true; //Charge the fee
+}
+
+
+
+//This function changes the coupon message in the checkout screen
+	
+add_filter( 'woocommerce_checkout_coupon_message', 'htg_rename_coupon_message_on_checkout' );
+
+function htg_rename_coupon_message_on_checkout() {
+	
+	 return '<a href="#" class="showcoupon">Redeeming A Hometown Giving Gift Credit? -- Click here to enter your code.</a>';
+	
+}
+
+
+
+//This function hides any memberships from the shop page
+
+add_action( 'pre_get_posts', 'custom_pre_get_posts_query' );
+
+function custom_pre_get_posts_query( $q ) {
+ 
+	if ( ! $q->is_main_query() ) return;
+	
+	if ( ! $q->is_post_type_archive() ) return;
+	
+	if ( ! is_admin() && is_shop() ) {
+	 
+		$q->set( 'tax_query', array(array(
+			
+			'taxonomy' => 'product_cat',
+			'field' => 'slug',
+			'terms' => array( 'memberships' ),  // Don't display products in the memberships category on the shop page
+			'operator' => 'NOT IN'
+		
+		)));
+		
+	}
+ 
+	remove_action( 'pre_get_posts', 'custom_pre_get_posts_query' );
+ 
+}
 
 
 /*
